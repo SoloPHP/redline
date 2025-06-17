8e2d731503e565e44eaff7a287d3cb2b92eb8c7f
SoloPHP <vedastudio@gmail.com>
2025-06-17 10:22:41 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Button, Card, Alert } from 'flowbite-svelte';
+	import { InfoCircleOutline } from 'flowbite-svelte-icons';
+</script>
+
+<div class="container mx-auto p-8">
+	<Card class="max-w-md mx-auto">
+		<Alert color="blue">
+			<InfoCircleOutline slot="icon" class="w-4 h-4" />
+			<span class="font-medium">Успех!</span>
+			SvelteKit + Tailwind v4 + Flowbite настроены
+		</Alert>
+
+		<h1 class="text-3xl font-bold text-center mb-4">
+			Проект готов! 🚀
+		</h1>
+
+		<Button class="w-full">
+			Супер кнопка Flowbite
+		</Button>
+	</Card>
+</div>
